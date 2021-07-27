698b925f352c6973a38e3db4ae91fb336c3c7045
Parviz Khavari <csctclan@gmail.com>
2021-07-27 22:37:19 +0300

    Registration: add time validation

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,3 +1,5 @@
+import pendulum
+
 from common import models as m
 from common.random import rand
 
@@ -6,6 +8,9 @@ class TestRegistration:
 
     def test_register(self, api):
         data = rand.object(m.RegisterReq)
+        creation_time = pendulum.now()
         res = api.register_req(data).structure(m.RegisterRes)
         assert res.status_code == 200
         assert res.data.to_type(m.RegisterIntermediate) == data.to_type(m.RegisterIntermediate)
+        assert creation_time.diff(res.data.created_at).in_seconds() < 1
+        assert res.data.updated_at is None
